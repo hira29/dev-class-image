bc18c6c3deeaf418778e3e55fd8dea98620728d7
hira29 <dzakydio@gmail.com>
2021-05-28 19:04:35 +0700

    Part 5

---
+++ src/pages/imagesPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class ImagesPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+
+    render() { 
+        return (  
+            <div className="container m-auto row" style={{paddingTop: '10vh'}}>
+                
+            </div>
+        );
+    }
+}
+ 
+export default ImagesPage;
